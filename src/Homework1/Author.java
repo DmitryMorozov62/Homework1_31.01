f645437999b5438427d20b4bc1865edd5837569c
DmitryMorozov62 <62morozov.dima.90@gmail.com>
2023-02-03 09:55:53 +0300

    Homework1.2(with methods: toString,equals,hashCode)

--- src/Homework1/Author.java
+++ src/Homework1/Author.java
@@ -1,5 +1,7 @@
 package Homework1;
 
+import java.util.Objects;
+
 public class Author {
      private String firstname;
      private String lastname;
@@ -15,4 +17,21 @@ public class Author {
         return this.lastname;
     }
 
+    @Override
+    public String toString() {
+        return  firstname + " " + lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(firstname, author.firstname) && Objects.equals(lastname, author.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstname, lastname);
+    }
 }
--- src/Homework1/Main.java
+++ src/Homework1/Main.java
@@ -1,19 +1,22 @@
 package Homework1;
 
+import java.util.Objects;
+
 public class Main {
 
     public static void main(String[] args) {
-        Book headFirst = new Book(new Author("Bert","Bates"),"Head First Java",2012);
-        System.out.println(headFirst.getAuthorName().getFirstnameAuthor() + " " + headFirst.getAuthorName().getLastnameAuthor()+ ", " +
-                headFirst.getNameBook() + ", " + headFirst.getYear());
+        Author headFirst1 = new Author("Bert", "Bates");
+        Author headFirst2 = new Author("Bert", "Bates");
 
-        headFirst.setYear(2017);
-        System.out.println(headFirst.getAuthorName().getFirstnameAuthor() + " " + headFirst.getAuthorName().getLastnameAuthor() + ", " +
-                headFirst.getNameBook() + ", " + headFirst.getYear());
+        Book headFirst = new Book(new Author("Bert", "Bates"), "Head First Java", 2012);
+        Book thinkingInJava = new Book(new Author("Bruce", "Eckel"), "thinking in java", 2015);
 
+        System.out.println(new Author("Bert", "Bates"));
+        System.out.println(new Book(new Author("Bert", "Bates"), "Head First Java", 2012));
+        System.out.println(new Author("Bruce", "Eckel"));
+        System.out.println(new Book(new Author("Bruce", "Eckel"), "thinking in java", 2015));
+        System.out.println(headFirst.equals(thinkingInJava));
+        System.out.println(headFirst1.equals(headFirst2));
 
-        Book thinkingInJava = new Book(new Author("Bruce","Eckel"),"thinking in java", 2015);
-        System.out.println(thinkingInJava.getAuthorName().getFirstnameAuthor() + " " + thinkingInJava.getAuthorName().getLastnameAuthor() + ", " +
-                thinkingInJava.getNameBook() + ", " + thinkingInJava.getYear());
     }
 }
